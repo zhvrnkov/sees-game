2dd0a52c8cb80c78fea4c1e4ecc89e23633cf48e
Vlad Zhavoronkov <v.zhvrnkv@gmail.com>
2021-10-09 02:52:24 +0600

    Revert "reduce number of j iterations"
    This reverts commit b423a5af590ce4243b16f74c980f0a1436b86825.

--- main.cpp
+++ main.cpp
@@ -20,21 +20,11 @@ Unit units[] = {
   { .pos_x = -5, .pos_y = -1, .dir_x = 0.7071067, .dir_y = 0.7071067 },
 };
 
-typedef struct {
-  bool val;
-  bool yal;
-} S;
-
-S sees(const Unit *src, const Unit *dest, const float distance, const float c) {
+bool sees(const Unit *src, const Unit *dest, const float distance, const float c) {
   float dp_x = dest->pos_x - src->pos_x;
   float dp_y = dest->pos_y - src->pos_y;
-  float sp_x = src->pos_x - dest->pos_x;
-  float sp_y = src->pos_y - dest->pos_y;;
   float dpl = sqrt(dp_x * dp_x + dp_y * dp_y);
-  return { 
-    .val = dpl <= distance && (src->dir_x * dp_x + src->dir_y * dp_y) >= c,
-    .yal = dpl <= distance && (dest->dir_x * sp_x + dest->dir_y * sp_y) >= c,
-  };
+  return dpl <= distance && (src->dir_x * dp_x + src->dir_y * dp_y) >= c;
 }
 
 
@@ -54,15 +44,11 @@ int main() {
   using std::chrono::milliseconds;
 
   auto t1 = high_resolution_clock::now();
-//  #pragma omp parallel for
+  #pragma omp parallel for
   for (int i = 0; i < us; i++) {
-    for (int j = i; j < us; j++) {
-      S x = sees(&uns[i], &uns[j], d, a);
-      if (x.val) {
+    for (int j = 0; j < us; j++) {
+      if (sees(&uns[i], &uns[j], d, a)) {
         uns[i].counter += 1;
-      }
-      if (x.yal) {
-        uns[j].counter += 1;
       }
     }
   }
