886b92aea8e2a2989e623aed9b1abed1b4feff7e
Vlad Zhavoronkov <v.zhvrnkv@gmail.com>
2021-10-09 18:20:49 +0600

    Revert "add counter as array of indices to Unit"
    This reverts commit f73d3d93472861a78857eb5ff4d063a1b1fc6727.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 typedef struct {
   vec2 pos;
   vec2 dir;
-  short counter[10000] = { 0 };
+  int counter;
 } Unit;
 
 float d = 20.0f;
@@ -20,18 +20,9 @@ Unit units[] = {
   { .pos = vec2(-5, -1), .dir = vec2(0.7071067f * d, 0.7071067f * d) },
 };
 
-typedef struct {
-  bool val;
-  bool yal;
-} S;
-
-S sees(const Unit *src, const Unit *dest, const float c, const float d) {
+bool sees(const Unit *src, const Unit *dest, const float c, const float d) {
   float dott = dot(src->dir, dest->pos - src->pos);
-  float dotv = dot(dest->dir, src->pos - dest->pos);
-  return {
-    .val = dott < d && dott >= c,
-    .yal = dotv < d && dotv >= c,
-  };
+  return dott < d && dott >= c;
 }
 
 int main() {
@@ -39,7 +30,7 @@ int main() {
   float c = d * a;
   size_t s = sizeof(units) / sizeof(Unit);
   size_t us = 10000;
-  Unit *uns = (Unit *)malloc(sizeof(Unit) * us);
+  Unit uns[us];
   for (int i = 0; i < us; i++) {
     uns[i] = units[i % s];
   }
@@ -53,13 +44,9 @@ int main() {
   auto t1 = high_resolution_clock::now();
   #pragma omp parallel for
   for (int i = 0; i < us; i++) {
-    for (int j = i; j < us; j++) {
-      S x = sees(&uns[i], &uns[j], c, ds);
-      if (x.val) {
-        uns[i].counter[j] = 1;
-      }
-      if (x.yal) {
-        uns[j].counter[i] = 1;
+    for (int j = 0; j < us; j++) {
+      if (sees(&uns[i], &uns[j], c, ds)) {
+        uns[i].counter += 1;
       }
     }
   }
@@ -68,9 +55,7 @@ int main() {
   std::cout << ms_double.count() << "ms" << endl;
   int counter = 0;
   for (int i = 0; i < us; i++) {
-    for (int j = 0; j < us; j++) {
-      counter += uns[i].counter[j];
-    }
+    counter += uns[i].counter;
   }
   std::cout << counter << endl;
 }
